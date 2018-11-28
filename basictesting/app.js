3571551fa02b18c0d169fbc9a1d236f0b3bef6df
Matheus Macedo <mathausmacedo@gmail.com>
2018-11-28 15:02:04 +0000

    basic testing

---
+++ basictesting/app.js
@@ -0,0 +1,30 @@
+var person = {
+  firstname: 'Matheus',
+  lastname: 'Macedo',
+  greet: function() {
+    console.log('Hello, ' + this.firstname + ' ' + this.lastname);
+  }
+}
+
+person.greet();
+console.log(person['firstname']);
+
+function Dog(name, breed) {
+  this.name = name;
+  this.breed = breed;
+}
+
+Dog.prototype.bark = function() {
+  console.log(this.name + ' barked!');
+}
+
+Dog.prototype.fbreed = function() {
+  console.log(this.name + ' is a ' + this.breed);
+}
+
+var blair = new Dog('Blair', 'Poodle');
+blair.bark();
+blair.fbreed();
+console.log(blair.__proto__);
+
+// objects are passed by reference
